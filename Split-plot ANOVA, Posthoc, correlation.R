5b2e9054a86deebf84188ac53f4feaeca25439c2
yansukmawan <66309330+yansukmawan@users.noreply.github.com>
2024-03-02 20:32:15 +0700

    Update Split-plot ANOVA, Posthoc, correlation.R

--- Split-plot ANOVA, Posthoc, correlation.R
+++ Split-plot ANOVA, Posthoc, correlation.R
@@ -5,13 +5,13 @@ split <- read_excel("D:/Analisis Statistika/RAKSplitPlot/split.xlsx")
 View(split)
 str(split)
 
-#mengubah struktur data
+##mengubah struktur data##
 split$Waktu<-as.factor(split$Waktu)
 split$Dosis<-as.factor(split$Dosis)
 split$Kelompok<-as.factor(split$Kelompok)
 str(split)
 
-#ANOVA dan Uji Lanjut dengan BNJ, BNT, DMRT
+##ANOVA dan Uji Lanjut dengan BNJ, BNT, DMRT##
 library(doebioresearch)
 #splitplot(data, block, main.plot, sub.plot, mean.comparison.test)#model yang digunakan
 splitplot(split[4],drip$Kelompok,drip$Waktu,drip$Dosis,1) #ANOVA-LSD untuk TT3
@@ -19,7 +19,7 @@ splitplot(split[4],drip$Kelompok,drip$Waktu,drip$Dosis,2) #ANOVA-DMRT untuk TT3
 splitplot(split[4],drip$Kelompok,drip$Waktu,drip$Dosis,3) #ANOVA-HSD untuk TT3
 splitplot(split[4:6],drip$Kelompok,drip$Waktu,drip$Dosis,2) #ANOVA-DMRT untuk TT3, DB3, dan JD3
 
-####analisis korelasi####
+##analisis korelasi##
 library(PerformanceAnalytics)
 my_dat<-split[, c(4,5,6)]
 chart.Correlation(my_dat, histogram = TRUE, pch=19)
